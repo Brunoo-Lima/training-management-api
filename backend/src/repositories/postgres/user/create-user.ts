31a44e5b9ffb3219697ac6e167c25445886ab343
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-03 17:27:25 -0300

    feat: add CreateUserRepository

---
+++ backend/src/repositories/postgres/user/create-user.ts
@@ -0,0 +1,8 @@
+import type { IUser } from '../../../@types/IUser';
+import { prisma } from '../../../lib/prisma';
+
+export class PostgresCreateUserRepository {
+  async execute(user: IUser) {
+    return await prisma.user.create({ data: user });
+  }
+}
