e9c3fcd804f1a307819f058306da1694b0d7f9d2
Bruno <Bruno2.jean.lima@outlook.com>
2026-05-05 14:27:43 -0300

    feat: add DeleteBookController

---
+++ api/src/controllers/book/delete-book.ts
@@ -0,0 +1,30 @@
+import type { Request } from 'express';
+import type { IDeleteBookUseCase } from '../../interfaces/use-cases';
+import { bookNotFoundResponse, ok, serverError } from '../helpers';
+import { BookNotFoundError } from '../../errors';
+
+export class DeleteBookController {
+  private deleteBookUseCase: IDeleteBookUseCase;
+
+  constructor(deleteBookUseCase: IDeleteBookUseCase) {
+    this.deleteBookUseCase = deleteBookUseCase;
+  }
+
+  async execute(request: Request) {
+    try {
+      const bookId = request.params.id as string;
+      const userId = request.userId as string;
+
+      const book = await this.deleteBookUseCase.execute(bookId, userId);
+
+      return ok(book);
+    } catch (error) {
+      if (error instanceof BookNotFoundError) {
+        return bookNotFoundResponse();
+      }
+
+      console.log(error);
+      return serverError();
+    }
+  }
+}
