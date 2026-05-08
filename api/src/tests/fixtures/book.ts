5ecf9d29d4ed3b8c6938a83c30f638d51df41870
Bruno <Bruno2.jean.lima@outlook.com>
2026-05-08 17:40:55 -0300

    feat: add fixtures books list

--- api/src/tests/fixtures/book.ts
+++ api/src/tests/fixtures/book.ts
@@ -14,3 +14,19 @@ export const book = {
   updated_at: faker.date.anytime(),
   user_id: faker.string.uuid(),
 };
+
+export const books = [
+  {
+    id: faker.string.uuid(),
+    title: faker.book.title(),
+    author: faker.person.fullName(),
+    genre: ['Fiction', 'Mystery', 'Thriller'],
+    status: StatusReading.WISHLIST,
+    total_pages: faker.number.int(),
+    start_date: faker.date.anytime(),
+    end_date: null,
+    created_at: faker.date.anytime(),
+    updated_at: faker.date.anytime(),
+    user_id: faker.string.uuid(),
+  },
+];
