ea2810aaddd4e14dd199ce2a505a39bff592ef0e
Bruno <Bruno2.jean.lima@outlook.com>
2026-05-18 17:17:11 -0300

    feat: add fixtures goals list

--- api/src/tests/fixtures/goal.ts
+++ api/src/tests/fixtures/goal.ts
@@ -11,3 +11,16 @@ export const goal = {
   created_at: faker.date.anytime(),
   updated_at: faker.date.anytime(),
 };
+
+export const goals = [
+  {
+    id: faker.string.uuid(),
+    type: GoalsType.DAILY_PAGES,
+    user_id: faker.string.uuid(),
+    target_value: faker.number.int(),
+    start_date: faker.date.anytime(),
+    end_date: null,
+    created_at: faker.date.anytime(),
+    updated_at: faker.date.anytime(),
+  },
+];
